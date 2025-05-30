778ce9d6881172587ec3825ebb7e075d4816b4a4
bbkjaz <kvasninai32@gmail.com>
2025-05-30 10:49:34 +0300

    админка

--- exams/admin.py
+++ exams/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import ikexam
 
-# Register your models here.
+class ikexamAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_date', 'exam_date', 'is_public')
+    
+    search_fields = ('title', 'users__email')
+    
+    list_filter = ('is_public', 'created_date', 'exam_date')
+    
+
+    filter_horizontal = ('users',)
+    
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'is_public')
+        }),
+        ('Даты', {
+            'fields': ('created_date', 'exam_date')
+        }),
+        ('Медиа', {
+            'fields': ('image',)
+        }),
+        ('Пользователи', {
+            'fields': ('users',)
+        }),
+    )
+    
+
+    date_hierarchy = 'exam_date'
+
+admin.site.register(ikexam, ikexamAdmin)
