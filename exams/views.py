dc6d03ca6614c28ffa27ce3a29b6e8be4c64e619
bbkjaz <kvasninai32@gmail.com>
2025-05-30 11:11:51 +0300

    страница

--- exams/views.py
+++ exams/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import ikexam
 
-# Create your views here.
+def exam_list(request):
+    exams = ikexam.objects.filter(is_public=True)
+    
+    context = {
+        'exams': exams,
+        'fio': 'Квашнина Ирина Александровна',
+        'group': '231-365'
+    }
+    return render(request, 'exams/exam_list.html', context)
--- myproject/urls.py
+++ myproject/urls.py
@@ -19,8 +19,11 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from exams import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('exams/', views.exam_list, name='exam_list'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
